9c0f3f0cb63885925184f1a653db83c87ee54cd0
giorgi _ <giksona151@gmail.com>
2023-12-14 15:27:55 +0400

    firstcommit

---
+++ Loginform/LoginForm.js
@@ -0,0 +1,25 @@
+const data = {
+    "photo_url": "../images/Libraryimage.jpg"
+};
+
+const photoUrl = data.photo_url;
+
+console.log(photoUrl);
+
+fetch('LoginForm.json')
+    .then(response => response.json())
+    .then(data => {
+        const photoUrl = data.photo_url;
+
+        const image = document.createElement('img');
+
+        image.src = photoUrl;
+
+        image.classList.add('header-image'); // Replace 'header-image' with your desired class name
+
+        const headerLeft = document.getElementById('headerLeft');
+        headerLeft.appendChild(image);
+    })
+    .catch(error => {
+        console.log('Error fetching data:', error);
+    });
